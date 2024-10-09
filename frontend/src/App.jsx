508656542be287d799ad5f4158a32016f7f60dea
Yago <yago.ks5@gmail.com>
2024-10-09 15:37:57 -0300

    Instalando react icons xd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,13 @@
+import Task from "./components/Task/Task";
+import Header from "./components/Header/Header";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header></Header>
+      <Task></Task>
+    </>
+  );
 }
 
 export default App;
