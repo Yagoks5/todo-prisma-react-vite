c63517410ab0a147d58d14e58e0263d737346526
Yago <yago.ks5@gmail.com>
2024-10-10 11:46:59 -0300

    instalando react-router e deletando os componentes. Limpando App.sx Comecar de novo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,7 @@
-import Header from "./components/Header/Header";
-import Tasks from "./components/Tasks/Tasks";
-import { useTasks } from "./hooks/useTasks";
-
 // LIMPAR AS FUNÇÕES E LIMPAR UM POUCO!!!!!!!!!!!!!
 
 function App() {
-  const {
-    activeTasks,
-    completedTasks,
-    newTask,
-    setNewTask,
-    error,
-    handleAddTask,
-    handleToggleTask,
-    handleDeleteTask,
-  } = useTasks();
-
-  return (
-    <>
-      <Header
-        onAddTask={handleAddTask}
-        newTask={newTask}
-        setNewTask={setNewTask}
-        error={error}
-      />
-      <Tasks
-        tasks={activeTasks}
-        onDelete={handleDeleteTask}
-        onComplete={handleToggleTask}
-        completedTasks={completedTasks}
-      />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
